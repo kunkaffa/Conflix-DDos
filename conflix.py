9643befabde42ed4d6e6ebc78170a239b21ef6d3
kunkaffa <kunkaffa3@gmail.com>
2025-09-29 23:56:12 +0700

    Update conflix.py

--- conflix.py
+++ conflix.py
@@ -80,10 +80,10 @@ def main():
         sys.exit(1)
 
     try:
-        port = int(input("\033[33m==⟩ IP [default 80]: \033[33m").strip() or 80)
+        port = int(input("\033[33m==⟩ IP [default 80]: \033[32m").strip() or 80)
         packet_size = int(input("\033[33m==⟩ Size [default 1490 bytes]: \033[32m").strip() or 1490)
-        threads = int(input("\033[33m==⟩ Number Of threads [default 4]: ").strip() or 4)
-        rate_limit = float(input("\033[33m==⟩ Duration [seconds]: ").strip() or 0.01)
+        threads = int(input("\033[33m==⟩ Number Of threads [default 4]: \033[32m").strip() or 4)
+        rate_limit = float(input("\033[33m==⟩ Duration [seconds]: \033[32m").strip() or 0.01)
     except ValueError:
         log_message("Invalid input provided. Exiting...")
         sys.exit(1)
